aeb96137e45278e925c73a9294733820d70fa22c
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-04-25 18:06:30 +0900

    fix

--- hosting/next.config.js
+++ hosting/next.config.js
@@ -1,9 +1,25 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
   trailingSlash: true,
+  assetPrefix: isProd ? "/dist" : undefined,
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // skip lint for build:watch
+  },
+  // trailingSlash: true,
+  // i18n: {
+  //   locales: ["en-US", "fr", "nl-NL"],
+  //   defaultLocale: "en-US",
+  // },
 };
 
 module.exports = nextConfig;
