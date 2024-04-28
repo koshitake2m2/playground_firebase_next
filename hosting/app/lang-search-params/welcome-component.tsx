3e2b91f138facd419b8dbcd15147792e5842daca
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-04-29 01:25:46 +0900

    fix meta

--- hosting/app/lang-search-params/welcome-component.tsx
+++ hosting/app/lang-search-params/welcome-component.tsx
@@ -5,7 +5,29 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { i18nMap } from "../../i18n/i18n";
 import { SwitchLangSearchParams } from "./switch-lang-search-params";
-import { LangType } from "../../i18n/i18n-type";
+import { I18nType, LangType } from "../../i18n/i18n-type";
+
+const metaProps = (i18n: I18nType) => [
+  { property: "og:type", content: "website" },
+  { property: "og:title", content: "playground-koshitake2m2" },
+  {
+    property: "og:description",
+    content: i18n.welcome,
+  },
+  { property: "og:site_name", content: "playground-koshitake2m2" },
+];
+
+const setMetadata = (i18n: I18nType) => {
+  if (typeof document !== "undefined" && !document.getElementById("og:title")) {
+    for (const metaProp of metaProps(i18n)) {
+      const element = document.createElement("meta");
+      element.setAttribute("id", metaProp.property);
+      element.setAttribute("property", metaProp.property);
+      element.setAttribute("content", metaProp.content);
+      document.head.appendChild(element);
+    }
+  }
+};
 
 export function WelcomeComponent() {
   const searchParams = useSearchParams();
@@ -13,12 +35,14 @@ export function WelcomeComponent() {
   const lang: LangType = searchParams.get("lang") === "ja" ? "ja" : "en";
   const i18n = i18nMap[lang];
 
+  setMetadata(i18n);
+
   return (
     <>
       <h1>lang</h1>
       <p>lang: {lang}</p>
       <p>welcome: {i18n.welcome}</p>
-      <Link href={`/lang-query-params/hello?lang=${lang}`}>hello page</Link>
+      <Link href={`/lang-search-params/hello?lang=${lang}`}>hello page</Link>
       <SwitchLangSearchParams />
     </>
   );
--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function () {
-  const linkList = ["/lang", "/lang-local-storage", "/lang-query-params"];
+  const linkList = ["/lang", "/lang-local-storage", "/lang-search-params"];
   const linkLiList = linkList.map((link) => (
     <li>
       <Link key={link} href={link}>
